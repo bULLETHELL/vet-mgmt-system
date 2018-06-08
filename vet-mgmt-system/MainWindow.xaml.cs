969cffc3e0fce37ce065f5a3e56e1da3ab95bbaa
Andreas Nordenhof <andreasnordenhof@gmail.com>
2018-06-08 10:44:20 +0200

    did stuff

--- vet-mgmt-system/MainWindow.xaml.cs
+++ vet-mgmt-system/MainWindow.xaml.cs
@@ -1,1 +1,98 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace vet_mgmt_system
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        TextBox firstNameTextBox = new TextBox();
+        TextBox lastNameTextBox = new TextBox();
+        TextBox zipCodeTextBox = new TextBox();
+        TextBox addressIDTextBox = new TextBox();
+
+        public void UserMgmtCreate_Click(object sender, RoutedEventArgs e)
+        {
+            Window createUserWindow = new Window();
+            createUserWindow.Width = 200;
+            createUserWindow.Show();
+            Button createButton = new Button() { Content = "Create" };
+            createButton.Click += CreateButton_Click;
+            var stackPanel = new StackPanel { Orientation = Orientation.Vertical };
+
+            stackPanel.Children.Add(new Label { Content = "First Name", HorizontalAlignment = HorizontalAlignment.Center });
+            stackPanel.Children.Add(firstNameTextBox);
+            stackPanel.Children.Add(new Label { Content = "Last Name", HorizontalAlignment = HorizontalAlignment.Center });
+            stackPanel.Children.Add(lastNameTextBox);
+            stackPanel.Children.Add(new Label { Content = "Zip Code", HorizontalAlignment = HorizontalAlignment.Center });
+            stackPanel.Children.Add(zipCodeTextBox);
+            stackPanel.Children.Add(new Label { Content = "Address ID", HorizontalAlignment = HorizontalAlignment.Center });
+            stackPanel.Children.Add(addressIDTextBox);
+            stackPanel.Children.Add(createButton);
+            createUserWindow.Content = stackPanel;
+        }
+        private void UserMgmtDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Window DeleteUserWindow = new Window();
+            DeleteUserWindow.Height = 400;
+            DeleteUserWindow.Width = 200;
+            DeleteUserWindow.Show();
+            Button deleteButton = new Button() { Content = "Delete" };
+            Button SearchButton = new Button() { Content = "Search" };
+            deleteButton.Click += DeleteButton_Click;
+            var stackPanel = new StackPanel { Orientation = Orientation.Vertical };
+            stackPanel.Children.Add(new Label { Content = "Search for a User" });
+            stackPanel.Children.Add(new Label { Content = "only delete a user if the user has no patients registered in the system" });
+            stackPanel.Children.Add(new TextBox { });
+            stackPanel.Children.Add(new DataGrid { });
+            
+          
+            DeleteUserWindow.Content = stackPanel;
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            //  Using the database
+            using (var context = new VetMgmtSystemDbEntities())
+            {
+                //  Make instance of owner object
+                var owner = new Owner()
+                {
+                    FirstName = firstNameTextBox.Text,
+                    LastName = lastNameTextBox.Text,
+                    ZipCode = Convert.ToInt32(zipCodeTextBox.Text),
+                    AddressID = Convert.ToInt32(addressIDTextBox.Text)
+                };
+
+                //  Add owner object to owners table
+                context.Owners.Add(owner);
+
+                //  Save changes to DB
+                context.SaveChanges();
+            }
+        }
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- vet-mgmt-system/VetMgmtSystemDb.Designer.cs
+++ vet-mgmt-system/VetMgmtSystemDb.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\SDE-04044\Documents\Github\vet-mgmt-system\vet-mgmt-system\vet-mgmt-system\VetMgmtSystemDb.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\andr439d\Source\Repos\vet-mgmt-system\vet-mgmt-system\VetMgmtSystemDb.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
